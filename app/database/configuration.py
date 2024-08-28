ce3630fc406815b1359816f56caf7d84947f8172
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-08-27 20:45:11 -0500

    ntf27/06/24 feat se crea variables de entorno para la conexion a la base de datos

---
+++ app/database/configuration.py
@@ -0,0 +1,11 @@
+# datos para la conexion a BD
+#variables de 
+dataBaseName = "gestordb"
+userName = "root"
+userPassword = ""
+connectionPort = 3306
+server = "localhost"
+
+#creando la conexion
+databaseConection = f"mysql+mysqlconnector://{userName}:{userPassword}@{server}:{connectionPort}/{dataBaseName}"
+print(databaseConection)
---
+++ main.py
@@ -0,0 +1,6 @@
+nombre = "Jason Steven"
+edad = 31
+
+print("mi nombre es: ",nombre,"y tengo: ",edad,"años")
+
+print(f"mi nombre es : {nombre} y tengo {edad} años")
